9a69b9a0e9481e87c6cb3b59af66fd2f55f3e220
Onyx Vega <mabs.24gl@gmail.com>
2024-05-08 16:31:45 -0400

    worked on image sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer className="footer">        
-      <div>Contact Info</div>
+      <div>Made Using React</div>
     </footer>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 // TODO
+import '../styles/Navbar.css';
+
 const styles = {
   card: {
     margin: 10,
@@ -10,13 +12,17 @@ const styles = {
     fontSize: '1.2rem',
     color: '#003b36',
     padding: '0 20px',
-  },
+  }, 
 };
 
 function Navbar() {
   return (
     <div style={styles.card}>
-      <div style={styles.heading}>Home</div>
+      <ul className='nav' style={styles.heading}>
+        <li>About Me</li>
+        <li>Contact</li>
+        <li>List of Works</li>
+        </ul>
     </div>
   );
 }
