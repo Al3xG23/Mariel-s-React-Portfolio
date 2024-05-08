6bb7b1b6a1696914a9f57d6832c47dbd3f2a7f6a
Onyx Vega <mabs.24gl@gmail.com>
2024-05-08 14:17:16 -0400

    added links to my deployed works

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@
 import '../styles/Card.css';
 
 const work = [
-  { title: "Travel Weather App", description: "A weather page that allows you to check the weather for multiple cities beore you travel."},
-  { title: "Password Generator", description: "A site that allows you to generate a secure password based on prompted criteria."},
-  { title: "Work Day Scheduler", description: "A simple scheduling site to organize your work day."},
-  { title: "Mariel's Note Taker", description: "A note taking site for your random and not so random thoughts."},
-  { title: "Budge It!", description: "A collaborative effort to create a budgeting app for your everyday life."},
-  { title: "Music2Me", description: "A collaborative page for journaling your take on music lyrics."},
+  { title: "Travel Weather App", description: "A weather page that allows you to check the weather for multiple cities beore you travel.", path: "https://al3xg23.github.io/Weather-App/"},
+  { title: "Password Generator", description: "A site that allows you to generate a secure password based on prompted criteria.", path: "https://al3xg23.github.io/password-generator/"},
+  { title: "Work Day Scheduler", description: "A simple scheduling site to organize your work day.", path:"https://al3xg23.github.io/calendar/"},
+  { title: "Mariel's Note Taker", description: "A note taking site for your random and not so random thoughts.", path: "https://mariel-note-taker-639db3e23d2a.herokuapp.com/"},
+  { title: "Budge It!", description: "A collaborative effort to create a budgeting app for your everyday life.", path: "https://blueberry-cake-79168-e1e68fe7cf3a.herokuapp.com/login"},
+  { title: "Music2Me", description: "A collaborative page for journaling your take on music lyrics.", path: "https://fairleyv.github.io/Music2Me/"}, 
 ];
 
 const styles = {
@@ -29,7 +29,7 @@ const styles = {
 function Card(props) {
   return(
     <div className='card' style={styles.card}>
-      <h2 style={styles.h2}>{props.title}</h2>
+      <a href={props.path} style={styles.h2}>{props.title}</a>
       <p style={styles.p}>{props.description}</p>
     </div>
   );
@@ -39,7 +39,7 @@ function CardList() {
   return (
     <div className='cardList'>
       {work.map((item, index) => (
-        <Card key={index} title={item.title} description={item.description} />
+        <Card key={index} title={item.title} path={item.path} description={item.description} />
       ))}
     </div>
   );
