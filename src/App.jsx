b42cc23e11cc3c1c0d3560fac8122f5be24d077f
Onyx Vega <mabs.24gl@gmail.com>
2024-05-08 13:58:28 -0400

    added some more files and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Navbar from './components/Navbar';
 import Header from './components/Header';
-import Card from './components/Card';
+// import Card from './components/Card';
+import CardList from './components/Card';
+import Footer from './components/Footer';
+
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
   return (
     <div>
+    
       <Header />
       <Navbar />
-      <Card />
-      <Card />
-      <Card />
+      <CardList />
+      <Footer/>
+      
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// TODO
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">        
+      <div>Contact Info</div>
+    </footer>
+  );
+}
+
+export default Footer;
